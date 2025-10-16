6c251e36ac0d9f45b95bb87e5dd27c7629a7b49b
Tiernan Messmer <tiernan.messmer@gmail.com>
2025-10-16 21:21:46 +1000

    expose empty type in dart sdk

--- dart/CHANGELOG.md
+++ dart/CHANGELOG.md
@@ -1,3 +1,7 @@
+## v1.0.6
+
+* Expose Empty type
+
 ## v1.0.5
 
 * Tweak discovery message
--- dart/lib/subspace_relay_pb.dart
+++ dart/lib/subspace_relay_pb.dart
@@ -1,3 +1,6 @@
 export 'src/nv/subspacerelay/subspacerelay.pb.dart';
 export 'src/nv/subspacerelay/subspacerelay.pbenum.dart';
 export 'src/nv/subspacerelay/subspacerelay.pbjson.dart';
+export 'src/google/protobuf/empty.pb.dart';
+export 'src/google/protobuf/empty.pbenum.dart';
+export 'src/google/protobuf/empty.pbjson.dart';
